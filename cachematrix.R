9f619bb59d3a130a6a5234f84f6cd7410af85878
xosemartinez <outromais@yahoo.es>
2015-01-24 23:14:28 +0100

    assignament 2: 1º intento

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This funcion create a new class of object, with a matrix and 2 internal functions
+#for to cache or search in cache their inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {  
+  if (nrow(x)==ncol(x)){
+    
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+  }else{message("this matrix isn't a square matrix")}
 }
 
 
-## Write a short comment describing this function
+## This function take a matrix transformed with makeCacheMatrix and search for their inverse in cache
+#and it computes the inverse, if you can't find it  
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
